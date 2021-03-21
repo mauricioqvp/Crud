40e820eac0ac87b610205a98fc9973b928688339
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-03-21 14:55:40 -0300

    Product, Category and Seed.

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_product (name, description, price, img_Url) VALUES ('Notebook Samsung', 'i7 4Gb Ram', 2300, 'https://images.tcdn.com.br/img/img_prod/15959/notebook_samsung_expert_350xbe_intel_core_i5_8a_g_8gb_ddr4_hd_1tb_placa_de_video_2gb_tela_15_6_windo_15817_1_20190920161419.jpg');
+
+INSERT INTO tb_category (name) VALUES ('Informatica');
+INSERT INTO tb_category (name) VALUES ('Eletrônicos');
+INSERT INTO tb_category (name) VALUES ('Livros');
